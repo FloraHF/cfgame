03141d37fb4491a0fedbfe3a3d010c3073ca3eb4
Flora <flora.fu@mail.utoronto.ca>
2019-11-08 11:51:17 -0500

    made animation

--- scripts/replay.py
+++ scripts/replay.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.patches import Circle
 import os
 import numpy as np
 from math import sin, cos, acos, atan2, sqrt, pi
@@ -16,12 +18,13 @@ class ReplayPool(object):
 		self._r = r 
 		self._r_close = 1.2*r
 		self._k_close = 0.9
+		self._dt = 0.1
 
 		self._t_start, self._t_end, self._t_close, self._fp = self._read_policy()
 		self._fx, self._fy = self._read_xy(self._t_start, self._t_end, file='location.csv')
 		self._fvx, self._fvy = self._read_xy(self._t_start, self._t_end, file='cmdVtemp.csv')
 		self._fa = self._read_a(self._t_start, self._t_end)
-		self._fh = self._read_heading(self._t_start, self._t_end)
+		self._fh = self._read_heading(self._t_start, self._t_end, file='analytic_heading.csv')
 
 		self._colors = ['b', 'g', 'r']
 
@@ -103,12 +106,12 @@ class ReplayPool(object):
 
 		return f_a
 
-	def _read_heading(self, t_start, t_end):
+	def _read_heading(self, t_start, t_end, file='actual_heading.csv'):
 		f_h = []
 		for cf in self._cfs:
 			t, h = [], []
 			data_dir = os.path.join(self._script_dir, self._res_dir + cf + '/data/')
-			with open(data_dir + 'actual_heading.csv') as f:
+			with open(data_dir + file) as f:
 			    data = f.readlines()
 			    for line in data:
 			        datastr = line.split(',')
@@ -174,7 +177,7 @@ class ReplayPool(object):
 
 	########################## policies ###########################
 	def _get_physical_heading(self, phi_1, phi_2, psi, D1_I, D2_I, D1_D2):
-		print('relative headings', phi_1*180/pi, phi_2*180/pi, psi*180/pi)
+		# print('relative headings', phi_1*180/pi, phi_2*180/pi, psi*180/pi)
 		dphi_1 = atan2(D1_I[1], D1_I[0])
 		dphi_2 = atan2(D2_I[1], D2_I[0])
 		dpsi = atan2(-D2_I[1], -D2_I[0])
@@ -182,7 +185,7 @@ class ReplayPool(object):
 		phi_1 += dphi_1
 		phi_2 += dphi_2
 		psi += dpsi
-		print('physical headings', phi_1*180/pi, phi_2*180/pi, psi*180/pi)
+		# print('physical headings', phi_1*180/pi, phi_2*180/pi, psi*180/pi)
 		return phi_1, phi_2, psi
 
 	def _h_strategy(self, xd1, yd1, xd2, yd2, xi, yi, a=.1/.15):
@@ -284,6 +287,7 @@ class ReplayPool(object):
 						fc='k', head_width=.01, zorder=10)
 				
 				phi_1, phi_2, psi = self._h_strategy(xd1, yd1, xd2, yd2, xi, yi, a=self._fa[i](t))
+				# phi_1, phi_2, psi = self._h_strategy(xd1, yd1, xd2, yd2, xi, yi)
 				headings_anl = self._get_physical_heading(phi_1, phi_2, psi, D1_I, D2_I, D1_D2)
 				heading = headings_anl[i]
 				vx, vy = cos(heading)/20, sin(heading)/20
@@ -309,11 +313,117 @@ class ReplayPool(object):
 		plt.show()
 
 
+	########################## plotting functions ########################
+	def animate_traj(self, end_stop=10):
+		n = int((self._t_end - self._t_start)/self._dt)+end_stop
+		times = np.concatenate((np.linspace(self._t_start, self._t_end, n-end_stop), self._t_end*np.ones(end_stop,)))
+		xs_exp = np.array([fx(times) for fx in self._fx])
+		ys_exp = np.array([fy(times) for fy in self._fy])
+		xmin, xmax = np.amin(xs_exp), np.amax(xs_exp)
+		ymin, ymax = np.amin(ys_exp), np.amax(ys_exp)
+		dx = (xmax - xmin)*0.2
+		dy = (ymax - ymin)*0.3
+
+		fig = plt.figure()
+		ax = fig.add_subplot(111, autoscale_on=True, xlim=(xmin-dx, xmax+dx), ylim=(ymin-dy, ymax+dy))
+		ax.set_aspect('equal')
+		ax.grid()
+		ax.tick_params(axis = 'both', which = 'major', labelsize = 16)
+		# plot_traj_phy(ax, xs_anl, line_style=(0, (5, 5)), dlabel='analytic', skip=50)
+		plt.xlabel('x', fontsize=16)
+		plt.ylabel('y', fontsize=16)
+
+		tail = int(n/5)
+
+		def generate_plot(ax, linestyle=(0, ()), alpha=0.5, label=None):
+			D1, = ax.plot([], [], 'o', color='b', label=None)
+			D2, = ax.plot([], [], 'o', color='b', label=None)
+			I, = ax.plot([], [], 'o', color='r', label=None)
+			D1tail, = ax.plot([], [], linewidth=2, color='b', linestyle=linestyle, label='Defender, '+label)
+			D2tail, = ax.plot([], [], linewidth=2, color='b', linestyle=linestyle, label=None)
+			Itail, = ax.plot([], [], linewidth=2, color='r', linestyle=linestyle, label='Defender, '+label)
+			Dline, = ax.plot([], [], '--', color='b', label=None)
+
+			D1cap = Circle((0, 0), self._r, fc='b', ec='b', alpha=alpha, label=None)
+			D2cap = Circle((0, 0), self._r, fc='b', ec='b', alpha=alpha, label=None)
+			ax.add_patch(D1cap)
+			ax.add_patch(D2cap)
+
+			return {
+						'D1': D1, 'D2': D2, 'I': I,
+						'D1tail': D1tail, 'D2tail': D2tail, 'Itail': Itail,
+						'Dline': Dline, 'D1cap': D1cap, 'D2cap': D2cap
+					}
+
+        # anl_plots = generate_plot(ax, linestyle=(0, (6, 5)), alpha=0.3)
+		exp_plots = generate_plot(ax, label='experiment')
+		plt.gca().legend(prop={'size': 12})
+
+		time_template = 'time = %.1fs'
+		time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes, fontsize=16)
+
+		def init():
+
+			def init_from_xs(plots, xs, ys):
+				plots['D1'].set_data([], [])
+				plots['D2'].set_data([], [])
+				plots['Dline'].set_data([], [])
+				plots['D1cap'].center = (xs[0], ys[0])
+				plots['D2cap'].center = (xs[1], ys[1])
+				plots['I'].set_data([], [])
+
+				plots['D1tail'].set_data([], [])
+				plots['D2tail'].set_data([], [])
+				plots['Itail'].set_data([], [])
+
+				return plots['D1'], plots['D2'], plots['D1cap'], plots['D2cap'], plots['I'], plots['D1tail'], plots['D2tail'], plots['Itail'], plots['Dline']
+
+            # objs_anl = init_from_xs(anl_plots, xs_anl)
+			objs_exp = init_from_xs(exp_plots, xs_exp[:,0], ys_exp[:,0])
+			time_text.set_text('')
+
+			return objs_exp + tuple((time_text,))
+
+		def animate(i):
+
+			def animate_for_xs(plots, xs, ys, i, ii):
+				# print(i, ys[0,i])
+				plots['D1'].set_data(xs[0,i], ys[0,i])
+				plots['D2'].set_data(xs[1,i], ys[1,i])
+				plots['Dline'].set_data([xs[0,i], xs[1,i]], [ys[0,i], ys[1,i]])
+				plots['D1cap'].center = (xs[0,i], ys[0,i])
+				plots['D2cap'].center = (xs[1,i], ys[1,i])
+
+				plots['I'].set_data(xs[2,i], ys[2,i])
+
+				plots['D1tail'].set_data(xs[0,ii:i+1], ys[0,ii:i+1])
+				plots['D2tail'].set_data(xs[1,ii:i+1], ys[1,ii:i+1])
+				plots['Itail'].set_data(xs[2,ii:i+1], ys[2,ii:i+1])
+
+				return plots['D1'], plots['D2'], plots['D1cap'], plots['D2cap'], plots['I'], plots['D1tail'], plots['D2tail'], plots['Itail'], plots['Dline']
+
+			# i = np.clip(0, 0, n-1)
+			i = i%n
+			ii = np.clip(i-tail, 0, i)
+			# print(ii, i)
+            # objs_anl = animate_for_xs(anl_plots, xs_anl, i, ii)
+			objs_exp = animate_for_xs(exp_plots, xs_exp, ys_exp, i, ii)
+			time_text.set_text(time_template % (times[i]))
+
+			return objs_exp + tuple((time_text, ))
+
+		ani = animation.FuncAnimation(fig, animate, init_func=init)
+		# ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+		#                                 repeat_delay=1000)
+		ani.save(self._res_dir+'ani_traj.gif')
+		# plt.show()
+
 ########################## main function ##########################
 if __name__ == '__main__':
 	cfs=['cf4', 'cf5', 'cf0']
-	res_dir='init_location_1/res1/'
+	res_dir='res1/'
 	r=.25
 
 	replayer = ReplayPool(cfs=cfs, res_dir=res_dir, r=r)
-	replayer.plot_traj()
+	# replayer.plot_traj()
+	replayer.animate_traj()
